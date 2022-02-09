997417900ae2baf400b90d3a2f6ac113038e575e
LasithEranga <lasitheranga1@gmail.com>
2022-02-10 00:28:13 +0530

    Interview section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import CardView from './components/CardView';
+import HorizontalView from './components/HorizontalCarousel';
 
 function App() {
   return (
+    <div>
     <Header/>
+    <p></p>
+    <HorizontalView />
+    </div>
   );
 }
 
