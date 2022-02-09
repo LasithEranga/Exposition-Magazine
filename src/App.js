4ad1e4a4c1180342aa476544672dcb23bd11a317
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-02-10 02:26:30 +0530

    feat: main image buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import LatestArticles from './components/LatestArticles';
-import MainImage from './components/MainImage';
-import CardView from './components/CardView';
-import HorizontalView from './components/HorizontalCarousel';
-
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import LatestArticles from "./components/LatestArticles";
+import MainImage from "./components/MainImage";
+import HorizontalView from "./components/HorizontalCarousel";
 
 function App() {
   return (
     <React.Fragment>
-      <Header/>
-      <LatestArticles/>
-      <MainImage/>
-     <div>
-    <Header/>
-    <p></p>
-    <HorizontalView />
-    </div>
+      <Header />
+      <LatestArticles />
+      <MainImage />
+
+      <div className="mt-4">
+        <HorizontalView />
+      </div>
     </React.Fragment>
   );
 }
--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -2,8 +2,19 @@ import image from '../images/main-image.png';
 import './MainImage.css';
 
 const MainImage = ()=>{
-    return <div>
+    return <div className='container-main-image'>
         <img src={image} alt="" className='mt-lg-2 main-image' style={{ width:'100%',objectFit:'cover'}}/>
+        <div className='blur-container-main-image'>
+            <div>
+            <a href='#home' className='border-0 btn text-light' style={{backgroundColor:'#F2B900'}} size="sm"> INTERVIEWS</a>
+            <a href='#gome' className='border-0 ms-2 btn text-light' style={{backgroundColor:'#F2B900'}} size="sm"> MAIN</a>
+            </div>
+            <div>
+                <div className='text-white' style={{fontSize:'2rem'}}>Deshabandu W. K. H. Wegapitiya</div>
+            </div>
+            
+        </div>
+        
     </div>
 }
 
