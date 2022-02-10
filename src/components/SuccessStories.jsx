60601c235c665c3a5d5e1c9f9e298b68dc786189
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-02-10 20:04:33 +0530

    feat: success stories

---
+++ src/components/SuccessStories.jsx
@@ -0,0 +1,8 @@
+import stories from '../Data/SuccessStories.json';
+
+import SuccessStoriesCarousel from './SuccessStoriesCarousel';
+const SuccessStories = ()=>{
+    return <SuccessStoriesCarousel stories={stories}/>
+}
+
+export default SuccessStories; 
---
+++ src/components/SuccessStoriesCarousel.jsx
@@ -0,0 +1,60 @@
+import { Carousel } from "react-bootstrap";
+import "./SuccessStories.css";
+
+const SuccessStoryCarousel = ({ stories }) => {
+  return (
+    <div className=" w-100">
+      <Carousel controls={false} fade className="position-relative">
+        {stories?stories.map((story,index) => (
+          <Carousel.Item key={index}>
+            <div className=" bg-dark w-100 " style={{ height: "40rem" }}>
+              <img
+                src={story.image?story.image:''}
+                alt={story.alt?story.alt:''}
+                className="w-100 h-100"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+            <div
+              className="position-absolute d-flex justify-content-center align-items-center flex-column flex-md-row"
+              style={{
+                width: "100%",
+                height: "100%",
+                margin: "auto",
+                top: "0",
+                left: "0",
+                backdropFilter: "saturate(180%) blur(10px)",
+                gap: "1rem",
+              }}
+            >
+              <img
+                style={{
+                  width: "15rem",
+                  height: "15rem",
+                  borderRadius: "15rem",
+                  objectFit: "cover",
+                }}
+                src={story.image?story.image:''}
+                alt={story.alt?story.alt:''}
+              />
+              <div
+                className="testomonial-width bg-light p-4 fst-italic d-flex justify-content-center align-items-center flex-column"
+                style={{
+                  borderRadius: "1.5rem",
+                  height: "fit-content"
+                }}
+              >
+                <p className="">{story.content?story.content:''}</p>
+                <div className="w-100 text-end fst-normal fw-bold">
+                  {story.name?story.name:''}
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+        )):''}
+      </Carousel>
+    </div>
+  );
+};
+
+export default SuccessStoryCarousel;
