4e455fee3b5c547c677d22eecbc5a9b12972d233
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-02-12 15:47:42 +0530

    feat auto scroll button

---
+++ src/components/Buttons/ScrollToTop.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { IoIosArrowUp } from "react-icons/io";
+
+const ScrollToTop = () => {
+  const [isScrolling, setisScrolling] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY>300?setisScrolling(true):setisScrolling(false);
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {
+      });
+    };
+  },[]);
+
+  const clickHandler = () => {
+    window.scrollTo({
+      top: 0,
+      left: 100,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    isScrolling && (
+      <div
+        className="d-flex justify-content-center align-items-center"
+        onClick={clickHandler}
+        style={{
+          width: "3rem",
+          height: "3rem",
+          borderRadius: "3rem",
+          position: "fixed",
+          bottom: "1rem",
+          right: "1.5rem",
+          backgroundColor: "#F2B900",
+          boxShadow: " 0px 8px 15px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <IoIosArrowUp size={30} style={{color:'white'}} />
+      </div>
+    )
+  );
+};
+
+export default ScrollToTop;
