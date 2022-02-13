439d94c59870632f78ac498a93ad1d290d734c63
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-02-13 14:49:46 +0530

    feat: latest article carosoul data loading

--- src/components/LatestArticlesSlider.jsx
+++ src/components/LatestArticlesSlider.jsx
@@ -1,17 +1,14 @@
 import { Carousel } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
+import ArticleList from "../Data/Articles.json";
 const LatestArticlesSlider = () => {
   return (
     <Carousel className="ms-3" controls={false} indicators={false}>
-      <Carousel.Item interval={2000} >
-       Lorem one 
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-      Lorem two 
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-      Lorem three
-      </Carousel.Item>
+      {
+        ArticleList.map((article,index)=>{
+          return <Carousel.Item key={index} interval={2000}><Link className=" ps-3 text-dark text-decoration-none" style={{fontSize:'0.9rem'}} to={`/articles/${article.id}`}>{article.title}<span className="text-muted "> - {article.date.toUpperCase()}</span></Link></Carousel.Item>
+        })
+      }
     </Carousel>
   );
 };
