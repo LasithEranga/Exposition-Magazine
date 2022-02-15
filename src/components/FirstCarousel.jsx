f6abee7c4b781077bda449b0fda145680848f2ce
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-02-15 20:20:25 +0530

    feat: expert insight data load

--- src/components/FirstCarousel.jsx
+++ src/components/FirstCarousel.jsx
@@ -1,20 +1,44 @@
 import React from "react";
-import FirstCarouselItem from "./FirstCarouselItem";
 import Card from "react-bootstrap/Card";
-import ExpertInsightCard from "./ExpertInsightCard";
+import { Col, Row } from "react-bootstrap";
+import "./FirstCarouselItem.css";
 
+import ArticleList from "../Data/Articles.json";
+import { Link } from "react-router-dom";
+
+let expertInsight = ArticleList.filter(
+  (article) => article.category === `expert's insights`
+);
 
 function FirstCarousel() {
   return (
     <Card className="rounded-0">
-      <Card.Body className="px-1 expert-card-hr">
-        <FirstCarouselItem />
-        <div className="mb-5"></div>
-        <ExpertInsightCard />
-      </Card.Body>
+      {expertInsight && expertInsight.length > 0 && (
+        <Link to={`/articles/${expertInsight[0].id}`}>
+          <Card.Body className="px-1 expert-card-hr">
+            <div className="expert-insight-img">
+              <img
+                alt=""
+                src={expertInsight[0].image}
+                className="w-100 h-100 expert-insight-img"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+            <div className="mb-5"></div>
+            <Row>
+              <div className="d-flex mt-1">
+                <Col className=" col-12 ms-lg-3 pb-5 text-dark">
+                  <h4>{expertInsight[0].title}</h4>
+                  <span>{expertInsight[0].description}</span>
+                </Col>
+              </div>
+              <div></div>
+            </Row>
+          </Card.Body>
+        </Link>
+      )}
     </Card>
   );
 }
-
 
 export default FirstCarousel;
